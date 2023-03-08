05a889fc6ac0394f74c905127745ee5282b69e3a
YG <yg@test.de>
2023-03-08 14:23:59 +0100

    wegspeichern damit ich pullen kann

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 import time
 
 # driver = webdriver.Edge(executable_path="C:\\Offline\\browserdrivers\\msedgedriver.exe")
-# driver = webdriver.Chrome(executable_path="C:\\Offline\\browserdrivers\\chromedriver.exe")
-driver = webdriver.Firefox(executable_path="C:\\Offline\\browserdrivers\\geckodriver.exe")
+driver = webdriver.Chrome(executable_path="C:\\Offline\\browserdrivers\\chromedriver.exe")
+# driver = webdriver.Firefox(executable_path="C:\\Offline\\browserdrivers\\geckodriver.exe")
 
 
 driver.get("https://www.google.de")
@@ -11,6 +11,6 @@ driver.maximize_window()
 print(driver.title)
 
 
-time.sleep(100)
+time.sleep(10)
 
 driver.close()
